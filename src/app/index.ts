fcb3dcd65519ff973b7e1fdfd6cb4a7036eca8bd
dt89 <tamalsarkartwoooo@gmail.com>
2026-03-30 11:13:39 +0000

    add: error and response

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import type Express from 'express';
+import ApiResponse from '../utils/api-response.js';
 
 export function createExpressApplication(): Express.Application {
   const app = express();
@@ -15,7 +16,7 @@ export function createExpressApplication(): Express.Application {
 
   //routes
   app.get('/', (req, res) => {
-    res.send('Hello World!');
+    ApiResponse.ok(res, 'Welcome to the API');
   });
 
   return app;
